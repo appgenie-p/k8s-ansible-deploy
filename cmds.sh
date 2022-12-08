133c12963c6a44ebde4e2d5562b58fcd3479ca59
Gennady Polezhaev <gpolejaev@gmail.com>
2022-12-08 13:56:13 +0300

    created playbook for local aliases config

--- cmds.sh
+++ cmds.sh
@@ -1,4 +1,5 @@
 # https://medium.com/linuxstories/vagrant-create-a-multi-machine-environment-b90738383a7e
+# ansible-playbook -i inventory.yaml git-install.yml -vv
 
 ssh vagrant@192.168.1.188 -i ~/.vagrant.d/insecure_private_key
 ansible all -m ping -i ansible_hosts.cfg
