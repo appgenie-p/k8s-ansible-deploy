02dd59125ae69cada424a742a77951b6277c9652
Gennady Polezhaev <gpolejaev@gmail.com>
2022-12-13 10:58:32 +0300

    remote-software with condition made

--- temp.py
+++ temp.py
@@ -1,37 +1,111 @@
-#!/usr/bin/env python3
-# NOTE: this file must be executable `chmod +x centos.py`
-	# challenge: make this file not executable and see what `ansible-inventory --list` prints
-# TEST this with a json formatter like `jq`:
-    # `./inventory_dir/centos.py | jq `
-# Great docs on how to build an inventory script
-# albeit an inventory plugin is preferrable
-# https://docs.ansible.com/ansible/latest/dev_guide/developing_inventory.html
-    # specifically about inventory scripts: https://docs.ansible.com/ansible/latest/dev_guide/developing_inventory.html#developing-inventory-scripts
-# https://docs.ansible.com/ansible/latest/user_guide/intro_dynamic_inventory.html
-import json
+# -*- coding: utf-8 -*-
 
-# must print a json dictionary, otherwise you'll get an error:
-#   " needs to be a json dict"
-inventory = {
-    # more about meta here https://docs.ansible.com/ansible/latest/dev_guide/developing_inventory.html#id16
-    "_meta": {
-        "hostvars": {}
-    },
-    "centos": {"hosts": []}
-}
+# Copyright: (c) 2017, Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-# programatically add hosts to inventory to show scripting benefits
-# challenge: increase range & Vagrantfile VMs to see how we can dynamically add more targets
-# challenge: port ubuntu static inventory to script too
-# challenge: port centos.py script (this file) back into static inventory 
-for i in range(2):
-    host = f"centos2{i}"
-    inventory["_meta"]["hostvars"][host] = {
-        "ansible_host": f"192.168.50.2{i}",
-        "ansible_private_key_file": f".vagrant/machines/{host}/virtualbox/private_key"
-    }
-    inventory["centos"]["hosts"].append(host)
+# This is a virtual module that is entirely implemented as an action plugin and runs on the controller
 
-# python json API https://docs.python.org/3/library/json.html
-# script returns inventory formatted as json to ansible
-print(json.dumps(inventory))
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+DOCUMENTATION = r'''
+---
+module: template
+version_added: historical
+short_description: Template a file out to a target host
+options:
+  follow:
+    description:
+    - Determine whether symbolic links should be followed.
+    - When set to C(true) symbolic links will be followed, if they exist.
+    - When set to C(false) symbolic links will not be followed.
+    - Previous to Ansible 2.4, this was hardcoded as C(true).
+    type: bool
+    default: no
+    version_added: '2.4'
+notes:
+- For Windows you can use M(ansible.windows.win_template) which uses C(\r\n) as C(newline_sequence) by default.
+- The C(jinja2_native) setting has no effect. Native types are never used in the C(template) module which is by design used for generating text files.
+  For working with templates and utilizing Jinja2 native types see the C(jinja2_native) parameter of the C(template lookup).
+seealso:
+- module: ansible.builtin.copy
+- module: ansible.windows.win_copy
+- module: ansible.windows.win_template
+author:
+- Ansible Core Team
+- Michael DeHaan
+extends_documentation_fragment:
+- action_common_attributes
+- action_common_attributes.flow
+- action_common_attributes.files
+- backup
+- files
+- template_common
+- validate
+attributes:
+    action:
+      support: full
+    async:
+      support: none
+    bypass_host_loop:
+      support: none
+    check_mode:
+      support: full
+    diff_mode:
+      support: full
+    platform:
+      platforms: posix
+    safe_file_operations:
+      support: full
+    vault:
+      support: full
+'''
+
+EXAMPLES = r'''
+- name: Template a file to /etc/file.conf
+  ansible.builtin.template:
+    src: /mytemplates/foo.j2
+    dest: /etc/file.conf
+    owner: bin
+    group: wheel
+    mode: '0644'
+
+- name: Template a file, using symbolic modes (equivalent to 0644)
+  ansible.builtin.template:
+    src: /mytemplates/foo.j2
+    dest: /etc/file.conf
+    owner: bin
+    group: wheel
+    mode: u=rw,g=r,o=r
+
+- name: Copy a version of named.conf that is dependent on the OS. setype obtained by doing ls -Z /etc/named.conf on original file
+  ansible.builtin.template:
+    src: named.conf_{{ ansible_os_family }}.j2
+    dest: /etc/named.conf
+    group: named
+    setype: named_conf_t
+    mode: 0640
+
+- name: Create a DOS-style text file from a template
+  ansible.builtin.template:
+    src: config.ini.j2
+    dest: /share/windows/config.ini
+    newline_sequence: '\r\n'
+
+- name: Copy a new sudoers file into place, after passing validation with visudo
+  ansible.builtin.template:
+    src: /mine/sudoers
+    dest: /etc/sudoers
+    validate: /usr/sbin/visudo -cf %s
+
+- name: Update sshd configuration safely, avoid locking yourself out
+  ansible.builtin.template:
+    src: etc/ssh/sshd_config.j2
+    dest: /etc/ssh/sshd_config
+    owner: root
+    group: root
+    mode: '0600'
+    validate: /usr/sbin/sshd -t -f %s
+    backup: yes
+'''
