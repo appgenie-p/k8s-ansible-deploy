992e1cb465b4f4c58b2b8208800651a175e02910
Gennady Polezhaev <gpolejaev@gmail.com>
2022-12-19 12:17:20 +0300

    Made containerd config file with shell module

--- cmds.sh
+++ cmds.sh
@@ -17,3 +17,11 @@ sudo sysctl -a | grep net.ipv4.ip_forward -A 5 -B 5
 deb amd64 https://download.docker.com/linux/ubuntu jammy stable
 
 sudo apt-get install containerd.io
+
+containerd config default | sudo tee /etc/containerd/config.toml
+
+ansible master -m shell -a 'containerd config default'
+
+apb playbooks/k8s-install.yaml --start-at-task 'Generate default file content' \
+-l master -CD
+
